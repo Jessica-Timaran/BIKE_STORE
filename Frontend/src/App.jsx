1fd72113ff4c82a0f76228f01a602288238b1a14
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-03-15 10:01:25 -0500

    nuevo repositorio
    react cartas

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Catalogo from './assets/components/Catalogo'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Catalogo/>
+   
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Catalogo from './assets/components/Catalogo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Catalogo />
+  </React.StrictMode>,
+)
