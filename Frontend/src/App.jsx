7dfd19140d1c8c31d7c100e1f117290a29a26141
YuranyRivera <160735735+YuranyRivera@users.noreply.github.com>
2024-03-15 11:46:33 -0500

    navbar
    actualizacion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <Catalogo/>
+        <Navbar/>
 
    
       </div>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Catalogo from './assets/components/Catalogo.jsx'
+import Navbar from './assets/components/Navbar.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Catalogo />
+    <Navbar />
   </React.StrictMode>,
 )
