b3103d377c047550fe48fcffea780cb08c0ab95a
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-03-15 11:14:38 -0500

    actualizacion
    de archivo e cartas css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div>
         <Catalogo/>
-        
+
    
       </div>
      
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import Catalogo from './assets/components/Catalogo.jsx'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Catalogo />
