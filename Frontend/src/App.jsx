bd48d04b2df2453d69d78a492aca437a4ea90999
Jessica-Timaran <jessicatimaran1@gmail.com>
2024-03-20 06:29:05 -0500

    footer
    css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
-import Catalogo from './assets/components/Catalogo'
+import Catalogo from './assets/components/Catalogo/Catalogo'
+import Filtro from './assets/components/Filtro/Filtro'
+import FondoBici from './assets/components/FondoBici/FondoBici'
+import Footer from './assets/components/Footer/Footer'
 
 
 function App() {
@@ -9,7 +12,9 @@ function App() {
     <>
       <div>
         <Catalogo/>
-
+        <Filtro/>
+        <FondoBici/>
+        <Footer/>
    
       </div>
      
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Catalogo from './assets/components/Catalogo.jsx'
+import Catalogo from './assets/components/Catalogo/Catalogo.jsx'
+import Filtro from './assets/components/Filtro/Filtro.jsx'
+import FondoBici from './assets/components/FondoBici/FondoBici.jsx'
+import Footer from './assets/components/Footer/Footer.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Catalogo />
+
+    <Footer/>
+
   </React.StrictMode>,
 )
