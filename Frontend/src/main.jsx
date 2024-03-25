9dcc9bb60af565f93a92a9e7c37a50447621f08a
YuranyRivera <160735735+YuranyRivera@users.noreply.github.com>
2024-03-25 14:29:10 -0500

    .
    .

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,6 +8,6 @@ import Navbar from './assets/components/Navbar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
+    <Navbar/>
   </React.StrictMode>,
 )
